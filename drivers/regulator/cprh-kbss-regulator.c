22f86f3ef721b9702c7cfff90f9fa7e2d0ce0d30
irfannim <irfannim.yt@gmail.com>
2021-01-08 10:28:21 +0700

    fix power for heavy loads (this should work)

--- drivers/regulator/cprh-kbss-regulator.c
+++ drivers/regulator/cprh-kbss-regulator.c
@@ -481,18 +481,18 @@ msm8998_v2_kbss_fuse_ref_volt[2][MSM8998_KBSS_FUSE_CORNERS] = {
 static const int
 sdm660_kbss_fuse_ref_volt[2][SDM660_KBSS_FUSE_CORNERS] = {
 	[CPRH_KBSS_POWER_CLUSTER_ID] = {
-		564000,
-		644000,
-		708000,
-		788000,
-		988000,
+		569000,
+		649000,
+		713000,
+		793000,
+		993000,
 	},
 	[CPRH_KBSS_PERFORMANCE_CLUSTER_ID] = {
-		644000,
-		708000,
-		788000,
-		908000,
-		988000,
+		649000,
+		713000,
+		793000,
+		913000,
+		913000,
 	},
 };
 
